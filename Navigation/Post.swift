01983e958e39b35f88136d89e586355e5f672b4c
vasilev <wasilievnikita@gmail.com>
2023-04-10 22:06:02 +0300

    first commit

---
+++ Navigation/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Никита Васильев on 06.04.2023.
+//
+
+import Foundation
+
+struct Post {
+var title: String?
+}
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Никита Васильев on 06.04.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGray
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+}
+
