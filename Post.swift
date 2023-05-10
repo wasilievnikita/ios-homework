c33cdbb20827987ef118cbbbc042161207c1165d
wasilievnikita <wasilievnikita@gmail.com>
2023-05-10 20:16:25 +0300

    full delete views

--- Post.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Post.swift
-//  Navigation
-//
-//  Created by Никита Васильев on 10.05.2023.
-//
-
-import Foundation
--- photos_for_collection.swift
+++
@@ -1,8 +0,0 @@
-//
-//  photos_for_collection.swift
-//  Navigation
-//
-//  Created by Никита Васильев on 10.05.2023.
-//
-
-import Foundation
